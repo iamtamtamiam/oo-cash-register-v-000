effdaad22e47f404221d3729daeb3e2c8d52ec91
Tam Vu <vutam_a@hotmail.com>
2019-06-22 21:30:13 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,21 @@
-
+class CashRegister
+  
+  attr_accessor :total, :discount
+  
+  
+  
+  def initialize(discount = 0)
+    @total = 0 
+    @discount =  discount
+    @items = []
+  end 
+  
+  def add_item(title, price, quantity = 1)
+    @total += price * quantity
+    @items << title
+  end 
+  
+  def apply_discount
+    
+  end 
+end 
